1f42ff88c775a7431c66910e92c4fcde4bd663a0
markodarko <markmcd26@gmail.com>
2021-08-24 01:49:03 -0400

    color changes

---
+++ colors.js
@@ -0,0 +1,24 @@
+const COLORS = {
+white1 	:'white'  ,
+whiteG 	:'#e3f1bf',
+whiteR 	:'#f9d7bd',
+black1 	:'#2c2c36',
+black2 	:'#161441',
+green0  :'#9ed329',
+green1 	:'#b1ff0e',
+green2 	:'#03c32c',
+yellow1	:'#ffff2d',
+blue1  	:'#8ac2bf',
+blue2  	:'#557c83',
+blue3	:'#286764',
+orange1	:'#ff952e',
+orange2	:'#f46634',
+orange3	:'#b70023',
+grey1  	:'#707070',
+grey2  	:'#565656'
+}
+
+const COLOR_SETS = {
+frog:
+  [COLORS.whiteG,COLORS.green2,COLORS.green1,COLORS.yellow1,COLORS.black2]
+}
