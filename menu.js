ed3882e075f164dd2c4a68b141002175d542297c
markodarko <markmcd26@gmail.com>
2021-08-28 21:44:20 -0400

    start button tweaks

--- menu.js
+++ menu.js
@@ -1,24 +1,27 @@
 class StartFrog{
   constructor(){
 	this.reset();
+	this.w = this.h = GRID_SIZE*2;
+	this.offsetX = Math.floor(this.w/4)
+	this.offsetY = Math.floor(this.h/4)
 	this.sprite = new Sprite(SPRITES.frog,COLOR_SETS.frog);
-	
 	this.floatMax = 1;
-	this.speed = 1;
-	this.time = 0;
-	this.w = this.h = GRID_SIZE;
+	
   }
   draw(){
-	this.sprite.draw(this.x,this.y+this.float)
+	//drawBox(this.x,this.y,this.w,this.h,'red')
+	this.sprite.draw(this.x+this.offsetX,this.y+this.offsetY + this.float)
   }
   update(){
 	if (this.y > C_HEIGHT) this.dead = 1;
 	if (this.start_pressed){
-		this.y++;
+		this.y += Math.floor(this.speed);
+		this.speed = Math.min(this.speed+.2, 3)
 		return;
 	}
 	if(collidePointCheck(swipeControl.endTouch,this)){
 		this.start_pressed = 1 ;
+		this.speed = -1.5;
 	}
 	
 	this.time++;
@@ -30,11 +33,13 @@ class StartFrog{
 	}
   }
   reset(){
+	this.speed = 1;
+	this.time = 0;
 	this.start_pressed = 0;
 	this.dead = 0;
 	this.dir = 1;
 	this.float = 0;
-	this.x = Math.floor(C_WIDTH/2-GRID_SIZE/2);
-	this.y = Math.floor(C_HEIGHT/2-GRID_SIZE/2);
+	this.x = Math.floor(C_WIDTH/2-GRID_SIZE);
+	this.y = Math.floor(C_HEIGHT/2-GRID_SIZE);
   }
 }
